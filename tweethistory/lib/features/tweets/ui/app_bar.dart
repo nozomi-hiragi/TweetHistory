8be9ef4cec4e8e42ee35689ba089c9860ad2a084
nozomi hiragi <nozomi.hiragi@gmail.com>
2025-06-10 23:40:29 +0900

    警告けし

--- tweethistory/lib/features/tweets/ui/app_bar.dart
+++ tweethistory/lib/features/tweets/ui/app_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../providers/tweet_select_provider.dart';
 import 'apply_tag_button.dart';
--- tweethistory/lib/main.dart
+++ tweethistory/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'features/tweets/tweets_screen.dart';
 import 'features/bin/bin_screen.dart';
