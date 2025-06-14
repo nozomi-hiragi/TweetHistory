4dea0f4859c38befae164d262877eead4df05d98
nozomi hiragi(ひいらぎのぞみ) <32630547+nozomi-hiragi@users.noreply.github.com>
2025-06-14 17:14:33 +0900

    Persist theme preference

--- tweethistory/lib/providers/theme_provider.dart
+++ tweethistory/lib/providers/theme_provider.dart
@@ -1,11 +1,36 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeModeNotifier extends Notifier<ThemeMode> {
-  @override
-  ThemeMode build() => ThemeMode.system;
+  SharedPreferences? _prefs;
 
-  void setDarkMode(bool isDark) => state = isDark ? ThemeMode.dark : ThemeMode.light;
+  @override
+  ThemeMode build() {
+    _load();
+    return ThemeMode.system;
+  }
+
+  Future<void> _load() async {
+    _prefs = await SharedPreferences.getInstance();
+    final index = _prefs!.getInt('themeMode');
+    if (index != null) {
+      state = ThemeMode.values[index];
+    }
+  }
+
+  Future<void> _save(ThemeMode mode) async {
+    final prefs = _prefs ??= await SharedPreferences.getInstance();
+    await prefs.setInt('themeMode', mode.index);
+  }
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    await _save(mode);
+    state = mode;
+  }
+
+  Future<void> setDarkMode(bool isDark) =>
+      setThemeMode(isDark ? ThemeMode.dark : ThemeMode.light);
 }
 
 final themeModeProvider = NotifierProvider<ThemeModeNotifier, ThemeMode>(
